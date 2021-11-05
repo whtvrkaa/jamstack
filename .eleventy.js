44377f6b88b44fd52c628eb232ea8db665441009
whtvrkaa <whtvrka@gmail.com>
2021-11-05 23:11:52 +0000

    .

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,8 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
-    
+    eleventyConfig.addPassthroughCopy('./src/admin');
+
 return {
     dir: {
         input: "src",
